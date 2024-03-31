68a02e3746ebbd06b3650fb1a157e8b35df1d50a
Chandima Ranaweera <chan4lk@gmail.com>
2024-03-31 08:39:44 +0530

    Fix build

--- prescription-builder/components/HeaderItem.tsx
+++ prescription-builder/components/HeaderItem.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-export const HeaderItem = ({ label, value, span = 1 }) => (
+export interface HeaderItemProps {
+    label: string
+    value: string
+    span?: number
+}
+
+export const HeaderItem: React.FC<HeaderItemProps> = ({ label, value, span = 1 }) => (
     <div className={`grid grid-cols-1 gap-1 col-span-${span}`}>
         <div>{label}</div>
         <div className='text-gray-500'>{value}</div>
--- prescription-builder/components/MediColumn.tsx
+++ prescription-builder/components/MediColumn.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
-export const MediColumn = ({ label, children, columnFor, span=1 }) => (
+export interface MediColumnProps {
+    label: string
+    children: React.ReactNode
+    columnFor: string
+    span?: number
+}
+
+export const MediColumn: React.FC<MediColumnProps> = ({ label, children, columnFor, span=1 }) => (
     <div className={`grid grid-cols-1 col-span-${span}`}>
         <label htmlFor={columnFor} className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
         <div className="relative mt-2 rounded-md shadow-sm">
